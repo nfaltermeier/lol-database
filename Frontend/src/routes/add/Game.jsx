127f71e976f243f323093ac22b78b17bc46a3bf5
Nick Monaco <nickjustpwnedu@gmail.com>
2021-11-14 07:16:04 -0600

    Reformatted Games.jsx spacing

--- Frontend/src/routes/add/Game.jsx
+++ Frontend/src/routes/add/Game.jsx
@@ -9,39 +9,39 @@ export default function Game() {
 	const [state, setState] = useState({ isLoading: true, isErrored: false, data: null });
 	const getData = useMemo(() => [{ key: 'teams', fn: () => (axios.get('http://localhost:28172/teams').then(data => data.data)) }], []);
 
-  return (
-    <main>
-      <h2>Game</h2>
-      <DataLoader state={state} setState={setState} actions={getData}>
-        <NoRedirectForm id="game" url="http://localhost:28172/games" method="post">
-          <div>
-            <label htmlFor="winningTeamID">Winning team</label>
-            <select name="winningTeamID" required>
-              <option value="">Please choose an option</option>
-              {state.data && state.data.teams.map(element => (
-                <option key={element.id} value={element.id}>{element.name}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="losingTeamID">Losing team</label>
-            <select name="losingTeamID">
-              <option value="">Please choose an option</option>
-              {state.data && state.data.teams.map(element => (
-                <option key={element.id} value={element.id}>{element.name}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="StartDateTime">Game start date time</label>
-            <DateTimeWrapper inputProps={{ name: 'StartDateTime', required: true }} initialViewDate={moment().hours(12).minutes(0).seconds(0)} />
-          </div>
-          <div>
-            <label htmlFor="nameabbreviation">Game duration</label>
-            <DateTimeWrapper inputProps={{ name: 'Duration', required: true }} dateFormat={false} timeFormat="HH:mm" />
-          </div>
-        </NoRedirectForm>
-      </DataLoader>
-    </main>
-  );
+	return (
+		<main>
+			<h2>Game</h2>
+			<DataLoader state={state} setState={setState} actions={getData}>
+				<NoRedirectForm id="game" url="http://localhost:28172/games" method="post">
+					<div>
+						<label htmlFor="winningTeamID">Winning team</label>
+						<select name="winningTeamID" required>
+							<option value="">Please choose an option</option>
+							{state.data && state.data.teams.map(element => (
+								<option key={element.id} value={element.id}>{element.name}</option>
+							))}
+						</select>
+					</div>
+					<div>
+						<label htmlFor="losingTeamID">Losing team</label>
+						<select name="losingTeamID">
+							<option value="">Please choose an option</option>
+							{state.data && state.data.teams.map(element => (
+								<option key={element.id} value={element.id}>{element.name}</option>
+							))}
+						</select>
+					</div>
+					<div>
+						<label htmlFor="StartDateTime">Game start date time</label>
+						<DateTimeWrapper inputProps={{ name: 'StartDateTime', required: true }} initialViewDate={moment().hours(12).minutes(0).seconds(0)} />
+					</div>
+					<div>
+						<label htmlFor="nameabbreviation">Game duration</label>
+						<DateTimeWrapper inputProps={{ name: 'Duration', required: true }} dateFormat={false} timeFormat="HH:mm" />
+					</div>
+				</NoRedirectForm>
+			</DataLoader>
+		</main>
+	);
 };
