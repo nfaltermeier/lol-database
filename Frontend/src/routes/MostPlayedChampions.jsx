f0f1ba301e519a1697cfb64a5fce31f52cb6367f
Pyrrhic <nefalt2@gmail.com>
2021-11-21 02:32:29 -0600

    no errors

--- Frontend/src/routes/MostPlayedChampions.jsx
+++ Frontend/src/routes/MostPlayedChampions.jsx
@@ -1,14 +1,16 @@
 import axios from 'axios';
 import classNames from 'classnames/bind';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import { useCallback, useMemo, useState } from 'react';
 import DataLoader from '../components/DataLoader';
 import DateTimeWrapper from '../components/DateTimeWrapper';
 import styles from './MostPlayedChampions.module.scss';
 
 export default function MostPlayedChampions() {
-	const [startDate, setStartDate] = useState('January 1 2000 01:00:00 -6:00');
-	const [endDate, setEndDate] = useState('January 1 2100 01:00:00 -6:00');
+	const tz = moment.tz.guess();
+	console.log(tz);
+	const [startDate, setStartDate] = useState(moment('10/4/2021 12:01 am -5:00', 'M/D/YYYY h:mm a Z').tz(tz).format('M/D/YYYY h:mm a Z'));
+	const [endDate, setEndDate] = useState(moment('11/7/2021 12:01 am -5:00', 'M/D/YYYY h:mm a Z').tz(tz).format('M/D/YYYY h:mm a Z'));
 	const [state, setState] = useState({ isLoading: true, isErrored: false, data: null });
 	const getData = useMemo(() => [
 		{ key: 'mostPlayedChampions', fn: () => (
@@ -18,7 +20,7 @@ export default function MostPlayedChampions() {
 	], [startDate, endDate]);
 	const onDateChange = useCallback((setMethod, value) => {
 		if (moment.isMoment(value)) {
-			setMethod(value.format('MMMM D YYYY h:mm:ss Z'));
+			setMethod(value.format('M/D/YYYY h:mm a Z'));
 		} else {
 			setMethod(value);
 		}
@@ -30,9 +32,9 @@ export default function MostPlayedChampions() {
 			<DataLoader state={state} setState={setState} actions={getData}>
 				<div>
 					<div>
-						<DateTimeWrapper inputProps={{ className: cx('datepicker') }} className={cx('datepicker-wrapper')} value={startDate} onChange={(v) => onDateChange(setStartDate, v)} initialViewDate={moment(startDate)} />
+						<DateTimeWrapper inputProps={{ className: cx('datepicker') }} className={cx('datepicker-wrapper')} value={moment(startDate, 'M/D/YYYY h:mm a Z')} onChange={(v) => onDateChange(setStartDate, v)} initialViewDate={moment(startDate, 'M/D/YYYY h:mm a Z')} />
 						{" through "}
-						<DateTimeWrapper inputProps={{ className: cx('datepicker') }} className={cx('datepicker-wrapper')} value={endDate} onChange={(v) => onDateChange(setEndDate, v)} initialViewDate={moment(endDate)} />
+						<DateTimeWrapper inputProps={{ className: cx('datepicker') }} className={cx('datepicker-wrapper')} value={moment(endDate, 'M/D/YYYY h:mm a Z')} onChange={(v) => onDateChange(setEndDate, v)} initialViewDate={moment(endDate, 'M/D/YYYY h:mm a Z')} />
 					</div>
 					<h3>Most Played Champions</h3>
 					<div>
