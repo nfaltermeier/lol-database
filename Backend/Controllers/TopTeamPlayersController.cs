1f5fa206db85817a839107df2e21cb3ab944eacd
Nick Monaco <nickjustpwnedu@gmail.com>
2021-11-27 21:52:06 -0600

    Finished TopTeamPlayers backend

---
+++ Backend/Controllers/TopTeamPlayersController.cs
@@ -0,0 +1,37 @@
+﻿using Backend.Database;
+using Backend.Model;
+using Backend.Model.Intermediate;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class TopTeamPlayersController : ControllerBase {
+        [HttpGet]
+        public ActionResult Get(DateTimeOffset StartDateTime, DateTimeOffset EndDateTime) {
+            if (!HttpContext.Request.Query.ContainsKey("StartDateTime")) {
+                return BadRequest("Query parameter 'StartDateTime' must be present");
+            } else if (!HttpContext.Request.Query.ContainsKey("EndDateTime")) {
+                return BadRequest("Query parameter 'EndDateTime' must be present");
+            }
+
+            List<SqlParameter> parameters = new()
+            {
+                new SqlParameter("@StartDateTime", StartDateTime),
+                new SqlParameter("@EndDateTime", EndDateTime)
+            };
+
+            using var reader = DatabaseConnector.RunStoredProcedure("LoLDB.[TopTeamPlayers]", parameters.ToArray());
+            List<TopTeamPlayers> results = new();
+            while (reader.Read())
+                results.Add(TopTeamPlayers.CreateTopTeamPlayers(reader));
+            return new ObjectResult(results);
+        }
+    }
+}
---
+++ Backend/Model/Intermediate/TopTeamPlayers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Model {
+    public record TopTeamPlayers(string PlayerName, string PlayerTeam, int KillCount, int? MostPlayedChampionID) {
+        public static TopTeamPlayers CreateTopTeamPlayers(SqlDataReader dataReader) {
+            return new TopTeamPlayers(dataReader.GetString(0),
+                                      dataReader.GetString(1),
+                                      dataReader.GetInt32(2),
+                                      dataReader.IsDBNull(3) ? null : dataReader.GetInt32(3));
+        }
+    }
+}
